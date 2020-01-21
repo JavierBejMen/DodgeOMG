b4cd9d4f4263b31e3980b471d694597167e3882d
Javier Bejar <j.bejar.mendez@gmail.com>
2020-01-21 18:17:38 +0100

    Done

---
+++ DodgeOMG/Mob.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+export var min_speed = 150  # Minimum speed range.
+export var max_speed = 250  # Maximum speed range.
+var mob_types = ["walk", "swim", "fly"]
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	
+func _on_start_game():
+    queue_free()
